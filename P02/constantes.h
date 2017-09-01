ca9aee482067d25e735d9d84aa797b93c3dc341b
Gilberto López <gilisaac@ciencias.unam.mx>
2017-08-31 22:29:51 -0500

    Práctica 2. Flex.

---
+++ P02/constantes.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdio.h>
+
+#define ENTERO 1
+#define IDENTIFICADOR 2
+#define CADENA 3
+#define KEYWORD 4
+#define BLANCO 5
+#define COMENTARIO 6
+#define OPERADOR 7
--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Laboratorio de compiladores
 
 * Prácticas:
   1. Sistema de procesamiento de Lenguaje.
+  2. Flex.
 
 * Tareas/
   + automata.c : Autómata que reconoce ID, enteros y flotantes.
