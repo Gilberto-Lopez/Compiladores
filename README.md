b5e7eb264649260a63093a09df01860a7109f83b
Gilberto López <gilisaac@ciencias.unam.mx>
2017-09-11 22:49:11 -0500

    Programa 2. Analizador léxico y sintáctico recursivo.

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ Laboratorio de compiladores
   + automata.c : Autómata que reconoce ID, enteros y flotantes.
   + ejemplo_lex.yy.c : Ejemplo de sintaxis para flex.
   + analizador_sintactico.l : Ejemplo de analizador sintáctico descendente recursivo.
+  + lex_sint.l : Analizador léxico y sintáctico. Programa 2.
---
+++ Tareas/defs.h
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define COMMA 1
+#define SCOLON 2
+#define INT 3
+#define FLOAT 4
+#define NUM 5
+#define ID 6
+#define ASSGN 7
+#define PLUS 8
+#define MINUS 9
+#define PROD 10
+#define DIV 11
+#define MOD 12
+#define LPAR 13
+#define RPAR 14
+
+void P (void);
+void D (void);
+void DP (void);
+void U (void);
+void L (void);
+void LP (void);
+void S (void);
+void SP (void);
+void E (void);
+void EP (void);
+void T (void);
+void TP (void);
+void F (void);
+void error (void);
