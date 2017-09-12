973250e53bac2ee6f29248723fb12c33499e3b9b
Gilberto López <gilisaac@ciencias.unam.mx>
2017-09-11 21:42:05 -0500

    Correcciones analizador sintáctico.

---
+++ Tareas/headers.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define MAS 1
+#define MUL 2
+#define ID 3
+#define LPAR 4
+#define RPAR 5
+
+void E (void);
+void EP (void);
+void T (void);
+void TP (void);
+void F (void);
+void error (void);
+void init (void);
